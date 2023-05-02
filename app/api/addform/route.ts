39aa25a12f9aec232977c0ea569a555379d844b9
Yousef-El-shr-bene <belaslion.gold@gmail.com>
2023-05-03 00:10:48 +0300

    new11

--- app/api/addform/route.ts
+++ app/api/addform/route.ts
@@ -2,7 +2,6 @@ import { addform } from "../../../firebase";
 import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const form = await request.json();
-
   await addform(
     form.ApartmentData,
     form.Feedback,
